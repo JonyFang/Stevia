167f56500789d8e93196aa7526cc2ab4c8604f58
JonyFang <jony.chunfang@gmail.com>
2024-03-24 13:14:00 +0800

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "Stevia",
+    platforms: [
+        .iOS(.v13),
+        .tvOS(.v13)
+    ],
+    products: [.library(name: "Stevia", targets: ["Stevia"])],
+    targets: [
+        .target(name: "Stevia", path: "Sources", resources: [.copy("PrivacyInfo.xcprivacy")]),
+        .testTarget(name: "SteviaTests", dependencies: ["Stevia"]),
+    ]
+)
